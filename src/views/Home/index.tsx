f828996647d2929726eafd87088c75c0e39c4946
7exch <97269532+7exch@users.noreply.github.com>
2022-07-07 12:03:45 +0530

    removed hero section

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -5,7 +5,6 @@ import useTheme from 'hooks/useTheme'
 import Container from 'components/Layout/Container'
 import { PageMeta } from 'components/Layout/Page'
 import { useTranslation } from 'contexts/Localization'
-import Hero from './components/Hero'
 import { swapSectionData, earnSectionData, cakeSectionData } from './components/SalesSection/data'
 import MetricsSection from './components/MetricsSection'
 import SalesSection from './components/SalesSection'
@@ -16,14 +15,6 @@ import CakeDataRow from './components/CakeDataRow'
 import { WedgeTopLeft, InnerWedgeWrapper, OuterWedgeWrapper, WedgeTopRight } from './components/WedgeSvgs'
 import UserBanner from './components/UserBanner'
 import MultipleBanner from './components/Banners/MultipleBanner'
-
-const StyledHeroSection = styled(PageSection)`
-  padding-top: 16px;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    padding-top: 48px;
-  }
-`
 
 const UserBannerWrapper = styled(Container)`
   z-index: 1;
@@ -51,25 +42,7 @@ const Home: React.FC = () => {
 
   return (
     <>
-      <PageMeta />
-      <StyledHeroSection
-        innerProps={{ style: { margin: '0', width: '100%' } }}
-        background={
-          theme.isDark
-            ? 'radial-gradient(103.12% 50% at 50% 50%, #21193A 0%, #191326 100%)'
-            : 'linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)'
-        }
-        index={2}
-        hasCurvedDivider={false}
-      >
-        {account && (
-          <UserBannerWrapper>
-            <UserBanner />
-          </UserBannerWrapper>
-        )}
-        <MultipleBanner />
-        <Hero />
-      </StyledHeroSection>
+      <PageMeta />      
       <PageSection
         innerProps={{ style: { margin: '0', width: '100%' } }}
         background={
