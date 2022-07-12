2b26a99c5bfc33966a83238ecad9dafa917813e1
7exch <97269532+7exch@users.noreply.github.com>
2022-07-12 12:59:38 +0530

    title, description and image path change

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -3,10 +3,10 @@ import { ContextApi } from 'contexts/Localization/types'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'PancakeSwap',
+  title: 'FUMA: SWAP any BEP20 Token',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+    'FUMA SWAP is designed by FUMA finance community. Swap any BEP20 Tokens instantly. Multichan Swap feature is coming shortly.',
+  image: 'https://www.fumaswap.com/images/hero.png',
 }
 
 interface PathList {
