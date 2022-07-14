e4c70be7cffab3a82f97d51ed0ef75ffd23045fc
7exch <97269532+7exch@users.noreply.github.com>
2022-07-14 16:25:33 +0530

    LogoWithTextIcon removed

--- packages/uikit/src/components/Footer/Footer.tsx
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -32,10 +32,7 @@ const MenuItem: React.FC<FooterProps> = ({
 }) => {
   return (
     <StyledFooter p={["40px 16px", null, "56px 40px 32px 40px"]} {...props} justifyContent="center">
-      <Flex flexDirection="column" width={["100%", null, "1200px;"]}>
-        <StyledIconMobileContainer display={["block", null, "none"]}>
-          <LogoWithTextIcon isDark width="130px" />
-        </StyledIconMobileContainer>        
+      <Flex flexDirection="column" width={["100%", null, "1200px;"]}>              
         <StyledSocialLinks order={[2]} pb={["42px", null, "32px"]} mb={["0", null, "32px"]} />
         <StyledToolsContainer
           order={[1, null, 3]}
