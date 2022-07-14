2c0698cef9f922b06bc6cff38b4d5bcef362a915
7exch <97269532+7exch@users.noreply.github.com>
2022-07-14 16:05:50 +0530

    link change

--- src/views/Home/components/Banners/LotteryBanner.tsx
+++ src/views/Home/components/Banners/LotteryBanner.tsx
@@ -102,7 +102,7 @@ const LotteryBanner = () => {
               <S.StyledHeading scale="xl">{t('Preparing')}</S.StyledHeading>
             </>
           )}
-          <NextLinkFromReactRouter to="https://khiladi.net">
+          <NextLinkFromReactRouter to="https://fumaforce.com/">
             <Button>
               <Text color="invertedContrast" bold fontSize="16px" mr="4px">
                 {status === FetchStatus.Fetched && isLotteryLive(data.status) ? 'Join Now' : 'Join Now'}
